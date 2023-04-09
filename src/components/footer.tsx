a78f6909b8ba5d7765a271aa51d7020dc6e152e3
umepy <haruya04183083@gmail.com>
2023-04-09 16:53:47 +0900

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="bg-sky-300">
+      <div className="flex justify-center">
+        <p className="text-sm text-gray-500 pt-10 pb-5">
+          Copyright © 2023, Haruya Umemoto
+        </p>
+      </div>
+    </div>
+  );
+}
