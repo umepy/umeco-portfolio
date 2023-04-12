c27cb3129ca4e476995ba25f8ba373b41242588d
umepy <haruya04183083@gmail.com>
2023-04-12 21:03:47 +0900

    created contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div className="flex justify-center overflow-hidden">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSddSfNEZpVb2oBJ_B1tmSJxz0lQ47xxOCXSXMW0glVF4uopaQ/viewform?embedded=true"
+        width="640"
+        height="800"
+        className="m-5"
+      >
+        読み込んでいます…
+      </iframe>
+    </div>
+  );
+}
