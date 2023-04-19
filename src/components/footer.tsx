99b66dc2f5a934fc3ffadcf9b29a827d6112ed9b
umepy <haruya04183083@gmail.com>
2023-04-19 22:56:00 +0900

    delete margin

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="bg-sky-300 mt-20">
+    <div className="bg-sky-300">
       <div className="flex justify-center">
         <p className="text-sm text-gray-500 pt-10 pb-5">
           Copyright © 2023, Haruya Umemoto
