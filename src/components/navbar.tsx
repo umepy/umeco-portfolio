e535194c501755c7c3c9f0d314ddbc9c09e4c28a
umepy <haruya04183083@gmail.com>
2023-04-19 22:56:50 +0900

    fix link bug which moves to abs link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="bg-sky-500 p-5">
       <div className="container flex flex-nowrap items-center justify-between mx-auto">
-        <a href="https://umeco.tokyo" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <Image
             src="/cat_icon_128.png"
             alt="umeco logo"
@@ -16,7 +16,7 @@ export default function Navbar() {
           <span className="text-2xl font-bold ml-2 text-gray-50">
             umeco&apos;s portfolio
           </span>
-        </a>
+        </Link>
         <div className="hidden w-full  md:block md:w-auto">
           <ul className="flex flex-row items-start justify-start p-4 mt-4 text-gray-50 font-bold md:space-x-8 md:mt-0 md:text-lg ">
             <li>
@@ -30,7 +30,7 @@ export default function Navbar() {
               </Link>
             </li>
             <li>
-              <Link href="/">
+              <Link href="/blog">
                 <p className="hover:scale-105 ">Blog</p>
               </Link>
             </li>
