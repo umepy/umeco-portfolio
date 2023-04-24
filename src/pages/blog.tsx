5cdf41f05c96672e69c91412f6fd3e7d9237b3d4
umepy <haruya04183083@gmail.com>
2023-04-24 23:52:17 +0900

    apply MyHead to all pages

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,7 @@
 import { InferGetStaticPropsType } from "next";
 import { getAllBlogsData } from "@/utils/blog_render";
-import Image from "next/image";
 import BlogCard from "@/components/blog_card";
+import { MyHead, HeadProps } from "@/components/myhead";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -15,8 +15,13 @@ export const getStaticProps = async () => {
 };
 
 export default function BlogPage({ blogs }: Props) {
+  const headprops: HeadProps = {
+    title: "ブログ記事一覧 | umeco's portfolio",
+    description: "umecoのブログ記事一覧ページ",
+  };
   return (
     <>
+      <MyHead {...headprops} />
       <div className="flex flex-wrap justify-center bg-gray-100">
         <div className="flex justify-center basis-full">
           <p className="text-2xl py-4">ブログ記事一覧</p>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,24 @@
+import { MyHead, HeadProps } from "@/components/myhead";
+
 export default function Contact() {
+  const headprops: HeadProps = {
+    title: "お問い合わせ | umeco's portfolio",
+    description:
+      "umecoのポートフォリオサイトです。最近のアウトプットやブログを公開しています。",
+  };
   return (
-    <div className="flex justify-center overflow-hidden">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSddSfNEZpVb2oBJ_B1tmSJxz0lQ47xxOCXSXMW0glVF4uopaQ/viewform?embedded=true"
-        width="640"
-        height="800"
-        className="m-5"
-      >
-        読み込んでいます…
-      </iframe>
-    </div>
+    <>
+      <MyHead {...headprops} />
+      <div className="flex justify-center overflow-hidden">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSddSfNEZpVb2oBJ_B1tmSJxz0lQ47xxOCXSXMW0glVF4uopaQ/viewform?embedded=true"
+          width="640"
+          height="800"
+          className="m-5"
+        >
+          読み込んでいます…
+        </iframe>
+      </div>
+    </>
   );
 }
