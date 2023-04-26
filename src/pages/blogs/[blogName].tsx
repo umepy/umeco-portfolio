8f8b451525b295cf93a842e70824f5a308de19ee
umepy <haruya04183083@gmail.com>
2023-04-26 21:05:54 +0900

    add link to blog home and horizontal line on ToC

--- src/pages/blogs/[blogName].tsx
+++ src/pages/blogs/[blogName].tsx
@@ -6,6 +6,7 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import { Link as ScrollLink } from "react-scroll";
 import { MyHead, HeadProps } from "@/components/myhead";
+import Link from "next/link";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -68,7 +69,12 @@ export default function BlogPage({ blog }: Props) {
       <div className="bg-gray-100">
         <div className="flex flex-row space-x-0 md:space-x-6 items-start justify-center py-10 md:px-4">
           <div className="shadow-lg rounded-md bg-white p-6 min-w-0 ">
-            <article className="prose prose-sm sm:prose prose-h2:after:prose-hr prose-code:before:hidden prose-code:after:hidden shrink">
+            <Link href="/blog">
+              <span className="hover:bg-gray-200 rounded-lg px-2">
+                🏠ブログ一覧
+              </span>
+            </Link>
+            <article className="prose prose-sm sm:prose prose-h2:after:prose-hr prose-code:before:hidden prose-code:after:hidden shrink pt-2">
               <ReactMarkdown
                 remarkPlugins={[remarkGfm, remarkMath]}
                 rehypePlugins={[rehypeKatex]}
@@ -78,10 +84,11 @@ export default function BlogPage({ blog }: Props) {
               </ReactMarkdown>
             </article>
           </div>
-          <div className="sticky top-4 shadow-lg rounded-md bg-white p-6 shrink-0 hidden md:block md:visible">
-            <div className="flex justify-center pb-4">
+          <div className="sticky top-4 shadow-lg rounded-md bg-white p-6 shrink-0 hidden md:block md:visible w-40">
+            <div className="flex justify-center">
               <p className="text-lg">目次</p>
             </div>
+            <div className="border-solid border-gray-200 border mb-4" />
             <ReactMarkdown
               allowedElements={["h2"]}
               components={{ h2: ankerLink }}
