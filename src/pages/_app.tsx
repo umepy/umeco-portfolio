c9d3f93af0baf6d57d803bd54b9aa6b79c31ca81
umepy <haruya04183083@gmail.com>
2023-05-06 15:18:35 +0900

    add google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { GAScript, pageview } from "@/utils/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Google Analytics Settings
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <GAScript />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
